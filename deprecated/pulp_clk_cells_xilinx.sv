9dd3169f14e74a2f9740771837340a638f9d31ee
galihat <103354074+galihat@users.noreply.github.com>
2026-01-22 15:34:12 +0700

    Add files via upload

---
+++ deprecated/pulp_clk_cells_xilinx.sv
@@ -0,0 +1,39 @@
+// Copyright 2019 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+// Cells to be used for Xilinx FPGA mappings
+
+
+
+module pulp_clock_inverter (
+  input  logic clk_i,
+  output logic clk_o
+);
+
+  assign clk_o = ~clk_i;
+
+endmodule
+
+module pulp_clock_mux2 (
+  input  logic clk0_i,
+  input  logic clk1_i,
+  input  logic clk_sel_i,
+  output logic clk_o
+);
+
+  BUFGMUX i_BUFGMUX (
+    .S  ( clk_sel_i ),
+    .I0 ( clk0_i    ),
+    .I1 ( clk1_i    ),
+    .O  ( clk_o     )
+  );
+
+endmodule
+
